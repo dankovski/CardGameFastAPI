429153920af9a20d80d08d9ce1009841989df279
unknown <cyprian.dankowski@gmail.com>
2023-02-20 22:30:38 +0100

    added alembic migrations

--- api/src/utils/user.py
+++ api/src/utils/user.py
@@ -10,7 +10,7 @@ class UserUtils:
     @classmethod
     def create_user(cls, db: Session, user: UserCreate):
         m = hashlib.sha256()
-        m.update(user.password + get_settings().SECRET_KEY)
+        m.update((user.password + get_settings().SECRET_KEY).encode())
         hashed_password = m.digest()
         db_user = UserModel(email=user.email, hashed_password=hashed_password, username=user.username)
         db.add(db_user)
@@ -19,9 +19,13 @@ class UserUtils:
         return db_user
 
     @classmethod
-    def get_user_by_id(cls: Session, user_id: int):
-        return cls.query(UserModel).filter(UserModel.id == user_id).first()
+    def get_user_by_id(cls, db: Session, user_id: int):
+        return db.query(UserModel).filter(UserModel.id == user_id).first()
 
     @classmethod
     def get_user_by_email(cls, db: Session, email: str):
         return db.query(UserModel).filter(UserModel.email == email).first()
+
+    @classmethod
+    def get_user_by_username(cls, db: Session, username: str):
+        return db.query(UserModel).filter(UserModel.username == username).first()
